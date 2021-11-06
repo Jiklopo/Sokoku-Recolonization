63f0750e22a4f5162166f55de49df0ef068eb933
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-11-06 19:01:53 +0600

    Initialize project

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class MainMenu : UIElement
+	{
+		[SerializeField] private Button playButton;
+		[SerializeField] private Button exitButton;
+
+		protected override void OnAwake()
+		{
+			playButton.onClick.AddListener(StartFirstLevel);
+			exitButton.onClick.AddListener(Quit);
+		}
+
+		private void StartFirstLevel()
+		{
+			SceneManager.LoadScene(1);
+		}
+
+		private void Quit()
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Scripts/UI/UIElement.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace UI
+{
+	public abstract class UIElement : MonoBehaviour
+	{
+		[SerializeField] private bool showAtStart;
+		[SerializeField] private float animationTime = 0.3f;
+
+		public bool IsShown => gameObject.activeSelf;
+		protected Action onClose;
+		protected UIElement lastCaller;
+
+		private void Awake()
+		{
+			OnAwake();
+		}
+
+		protected virtual void OnAwake()
+		{
+		}
+
+		private void Start()
+		{
+			gameObject.SetActive(showAtStart);
+		}
+
+		public void Show(UIElement caller)
+		{
+			lastCaller = caller;
+			caller.Close();
+			Show();
+		}
+
+		public void Show()
+		{
+			gameObject.SetActive(true);
+			transform.localScale = Vector3.zero;
+			transform.DOScale(Vector3.one, animationTime).OnComplete(OnShown);
+		}
+
+		protected virtual void OnShown()
+		{
+		}
+
+		public void Close(Action onClose = null)
+		{
+			transform
+				.DOScale(Vector3.zero, animationTime)
+				.OnComplete(() =>
+				{
+					gameObject.SetActive(false);
+					OnClosed();
+				});
+
+			lastCaller?.Show();
+			lastCaller = null;
+			onClose?.Invoke();
+		}
+
+		protected virtual void OnClosed()
+		{
+		}
+	}
+}
