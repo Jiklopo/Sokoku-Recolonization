315a2568b64805b48ba81b512ce1c8a5c4720e4d
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-11-07 11:27:53 +0600

    Add inventory controls

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using InventorySystem;
+using UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+	[RequireComponent(typeof(Player))]
+	public class PlayerInventory : MonoBehaviour
+	{
+		[SerializeField] private int maxSize;
+		
+		private Player player;
+		private Inventory inventory;
+
+		private void Awake()
+		{
+			player = GetComponent<Player>();
+			inventory = new Inventory(maxSize);
+		}
+
+		private void Start()
+		{
+			InventoryPanel.Instance.Initialize(maxSize);
+			player.InputActions.UI.Inventory.performed += ToggleInventory;
+		}
+
+		private void ToggleInventory(InputAction.CallbackContext context)
+		{
+			InventoryPanel.ToggleInstance();
+		}
+	}
+}
