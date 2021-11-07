5480e99738c213e91fec51cc8963ee73beba0967
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-11-07 11:25:24 +0600

    Add inventory panel

---
+++ Assets/Scripts/UI/InventoryItem.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class InventoryItem : MonoBehaviour
+	{
+		[SerializeField] private Image itemImage;
+		[SerializeField] private TextMeshProUGUI countText;
+		private ItemData data;
+
+		public void SetData(ItemData itemData, int amount)
+		{
+			itemImage.sprite = itemData.icon;
+			countText.SetText(amount.ToString());
+		}
+
+		public void Clear()
+		{
+			itemImage.sprite = null;
+			countText.SetText("");
+		}
+	}
+}
---
+++ Assets/Scripts/UI/InventoryPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Data;
+using Events;
+using Extensions;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class InventoryPanel: UIElementSingleton<InventoryPanel>
+	{
+		[SerializeField] private InventoryItem inventoryItemPrefab;
+		[SerializeField] private GridLayoutGroup inventoryGrid;
+
+		private List<InventoryItem> items = new List<InventoryItem>();
+		protected override void OnAwake()
+		{
+			base.OnAwake();
+			GameBus.OnInventoryUpdated += UpdateInventoryGrid;
+		}
+
+		public void Initialize(int slotsAmount)
+		{
+			items.Clear();
+			inventoryGrid.transform.DestroyAllChildren();
+
+			for (var i = 0; i < slotsAmount; i++)
+			{
+				items.Add(Instantiate(inventoryItemPrefab, inventoryGrid.transform));
+				items[i].Clear();
+			}
+		}
+
+		private void UpdateInventoryGrid(Dictionary<ItemData, int> inventory)
+		{
+			inventoryGrid.transform.DestroyAllChildren();
+			foreach (var inventoryItem in items)
+				inventoryItem.Clear();
+
+			var i = 0;
+			foreach (var itemData in inventory)
+			{
+				items[i++].SetData(itemData.Key, itemData.Value);
+				if (i > items.Count)
+					break;
+			}
+		}
+	}
+}
