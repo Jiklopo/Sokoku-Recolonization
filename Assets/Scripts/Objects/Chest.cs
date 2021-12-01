bf6253962ba3f8a172d7ac40b49851c0dba74e2b
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-12-01 23:05:36 +0600

    Add debug chest

---
+++ Assets/Scripts/Objects/Chest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace InventorySystem
+{
+	public class Chest : MonoBehaviour, ICollisionTarget
+	{
+		[SerializeField] private Vector3 itemOffset;
+
+		private bool isUsed;
+		public void OnCollision(GameObject other)
+		{
+			if (isUsed)
+				return;
+
+			var item = ItemFactory.Instance.GetRandomItem(1);
+			if (item == null)
+				return;
+			
+			item.transform.position = transform.position + Vector3.up;
+			isUsed = true;
+		}
+	}
+}
