2fda3d965bd2bcf70f93a6ab848d7612da192b6d
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-12-01 22:01:25 +0600

    Add generic singleton class

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utilities
+{
+	public abstract class Singleton<T> : MonoBehaviour where T: Component
+	{
+		public static T Instance { get; protected set; }
+		
+		protected virtual void Awake()
+		{
+			if (Instance != null)
+			{
+				Debug.LogWarning($"Instance of {GetType()} already exists! Destroying {name}.");
+				Destroy(this);
+			}
+
+			Instance = this as T;
+			DontDestroyOnLoad(this);
+		}
+	}
+}
