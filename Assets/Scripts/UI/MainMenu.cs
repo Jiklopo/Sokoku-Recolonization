5537f3f39315fdf7c5fd5de99586e26b6398be52
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2021-12-12 17:59:35 +0600

    Add MainMenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,18 +7,25 @@ namespace UI
 	public class MainMenu : UIElementSingleton<MainMenu>
 	{
 		[SerializeField] private Button playButton;
+		[SerializeField] private Button settingsButton;
 		[SerializeField] private Button exitButton;
 
 		protected override void OnAwake()
 		{
 			base.OnAwake();
 			playButton.onClick.AddListener(StartFirstLevel);
+			settingsButton.onClick.AddListener(OpenSettings);
 			exitButton.onClick.AddListener(Quit);
 		}
 
 		private void StartFirstLevel()
 		{
 			SceneManager.LoadScene(1);
+		}
+
+		private void OpenSettings()
+		{
+			SettingsScreen.Instance.Show(this);
 		}
 
 		private void Quit()
